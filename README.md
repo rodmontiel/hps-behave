9a1a806746d836092140c4bfc3a37c7937d608fa
Vincent Pretre <vincent.pretre@gmail.com>
2016-02-24 10:42:29 +0100

    Integrate Travis, update Readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # hps-behave
+[![Build Status](https://travis-ci.org/hiptest/hps-behave.svg?branch=master)](https://travis-ci.org/hiptest/hps-behave)
+
 Hiptest publisher samples with Behave
+
+In this repository you'll find tests generated in Behave format from [Hiptest](https://hiptest.net), using [Hiptest publisher](https://github.com/hiptest/hiptest-publisher).
+
+The goals are:
+
+ * to show how tests are exported in Behave.
+ * to check exports work out of the box (well, with implemented actionwords)
+
+System under test
+------------------
+
+The SUT is a (not that much) simple coffee machine. You start it, you ask for a coffee and you get it, sometimes. But most of times you have to add water or beans, empty the grounds. You have an automatic expresso machine at work or at home? So you know how it goes :-)
+
+Update tests
+-------------
+
+
+To update the tests:
+
+    hiptest-publisher -c behave.conf --only=features,step_definitions
+
+The tests are generated in the [``features``](https://github.com/hiptest/hps-behave/tree/master/features) directory.
+
+Run tests
+---------
+
+
+To build the project and run the tests, use the following command:
+
+    behave --junit --junit-directory results
+
+The SUT implementation can be seen in [``src/coffee_machine.py``](https://github.com/hiptest/hps-behave/blob/master/src/coffee_machine.py)
+
+The test report is generated in ```results/```
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -11,79 +11,62 @@ use_step_matcher('re')
 
 @given(r'I start the coffee machine "(.*)"')
 @when(r'I start the coffee machine "(.*)"')
-
 def impl(context, lang = "en"):
     context.actionwords.i_start_the_coffee_machine(lang)
 
 
 @when(r'I shutdown the coffee machine')
-
 def impl(context):
     context.actionwords.i_shutdown_the_coffee_machine()
 
 
 @then(r'message "(.*)" should be displayed')
-
 def impl(context, message):
     context.actionwords.message_message_should_be_displayed(message)
 
 
 @then(r'coffee should be served')
-
 def impl(context):
     context.actionwords.coffee_should_be_served()
 
 
 @then(r'coffee should not be served')
-
 def impl(context):
     context.actionwords.coffee_should_not_be_served()
 
 
 @when(r'I take a coffee')
-
 def impl(context):
     context.actionwords.i_take_a_coffee()
 
 
 
-def impl(context):
-    context.actionwords.i_empty_the_coffee_grounds()
-
 
 @when(r'I fill the beans tank')
-
 def impl(context):
     context.actionwords.i_fill_the_beans_tank()
 
 
 @when(r'I fill the water tank')
-
 def impl(context):
     context.actionwords.i_fill_the_water_tank()
 
 
 @when(r'I take "(.*)" coffees')
-
 def impl(context, coffee_number):
     context.actionwords.i_take_coffee_number_coffees(coffee_number)
 
 
 @given(r'the coffee machine is started')
-
 def impl(context):
     context.actionwords.the_coffee_machine_is_started()
 
 
 @when(r'fifty coffees have been taken without filling the tank')
-
 def impl(context):
     context.actionwords.fifty_coffees_have_been_taken_without_filling_the_tank()
 
 
 @when(r'thirty eight coffees are taken without filling beans')
-
 def impl(context):
     context.actionwords.thirty_eight_coffees_are_taken_without_filling_beans()
-
-
